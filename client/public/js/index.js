006db6011404451a6b1089577f36291d24080b93
Zack Jackson <z.jackson58@gmail.com>
2019-01-17 16:37:35 -0500

    Autoscroll to bottom for new messages

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -1,5 +1,26 @@
 const socket = io();
 
+// Autoscroll
+const scrollToBottom = () => {
+  // Selectors
+  const messages = jQuery('#messages');
+  const newMessage = messages.children('li:last-child');
+
+  // Heights
+  const clientHeight = messages.prop('clientHeight');
+  const scrollTop = messages.prop('scrollTop');
+  const scrollHeight = messages.prop('scrollHeight');
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (
+    clientHeight + scrollTop + newMessageHeight + lastMessageHeight >=
+    scrollHeight
+  ) {
+    messages.scrollTop(scrollHeight);
+  }
+};
+
 // Listen for connection to the server
 socket.on('connect', () => {
   console.log('Connected to server');
@@ -21,6 +42,7 @@ socket.on('newMessage', ({ from, body, createdAt }) => {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', e => {
